3a9eeb191331d63e9acf4ce809200ee3fc93ab50
Justmufid <98690718+Justmufid@users.noreply.github.com>
2024-09-03 12:44:45 +0700

    User (#10)
    * membuat crud user
    
    * crud user

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,13 +2,14 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
+    
     use HasFactory, Notifiable;
 
     /**
@@ -16,11 +17,11 @@ class User extends Authenticatable
      *
      * @var array<int, string>
      */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
+    protected $table = 'users';
+    protected $primaryKey = 'id';
+    protected $fillable = ['username','password','role','email','verify_key','active'];
+    protected $attributes = ['active' => 1,];
+    
 
     /**
      * The attributes that should be hidden for serialization.
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,31 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\HomeController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::get('/', [HomeController::class, 'dashboard'])->name('dashboard');
+
+Route::get('/index', [HomeController::class, 'index'])->name('index');
+
+Route::get('/create', [HomeController::class, 'create'])->name('create');
+
+Route::post('/store', [HomeController::class, 'store'])->name('store');
+
+Route::get('/edit/{id}', [HomeController::class, 'edit'])->name('edit');
+Route::put('/update/{id}', [HomeController::class, 'update'])->name('update');
+Route::delete('/delete/{id}', [HomeController::class, 'delete'])->name('delete');
+
+
+Route::get('home', [LoginController::class, 'login'])->name('login');
+Route::post('actionlogin', [LoginController::class, 'actionlogin'])->name('actionlogin');
+
+
+Route::get('home', [HomeController::class, 'index'])->name('home')->middleware('auth');
+Route::get('actionlogout', [LoginController::class, 'actionlogout'])->name('actionlogout')->middleware('auth');
+
+Route::get('register', [RegisterController::class, 'register'])->name('register');
+Route::post('register/action', [RegisterController::class, 'actionregister'])->name('actionregister');
+
